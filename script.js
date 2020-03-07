4a185a7697752f483fbaa7144f30803561d87390
Joshua Chu <jashya@gmail.com>
2020-03-07 12:32:58 -0500

    Add detailed ingredeints functionality

--- script.js
+++ script.js
@@ -21,29 +21,52 @@ function recipeURL(recipeID) {
 
 // UNFINISHED
 // Ingredient list function
-// Takes 1 argument, an object of 'recipe info information'
+// Takes 1 argument, an array of 'extended ingredients'
 // Outputs an array with the list of 'ingredient' objects
 function ingredientList(recipeInfo) {
 
 }
 
 // Basic recipe card display function
-// Takes 1 argument, a recipe object
+// Takes 1 argument, a basic recipe object
 // Appends a completed card to the display area
 function buildRecipeCard(recipe) {
   var recipeTitle = recipe.title;
   var recipeImage = recipeURL(recipe.id);
-  var recipeCard = $("<div class \"recipe-card\"></div>");
+  var recipeCard = $("<div class=\"recipe-card\"></div>");
   recipeCard.append("<div class =\"recipe-card-thumbnail\"><img src=\"" + recipeImage + "\"><h2 class=\"recipe-name\"><a href=\"#\">" + recipeTitle + "</a></h2><span class=\"recipe-source\">Recipe Source</span><div class=\"recipe-card-footer-bar\"><button href=\"#\" class=\"product-card-color-option\"><i data-id=\"" + recipe.id + "\" class=\"fas fa-info-circle\"></i></button><button href=\"#\" class=\"product-card-color-option\"><i data-id=\"" + recipe.id + "\" class=\"fas fa-plus\"></i></button></div></div>");
   $("#recipe-display").append($(recipeCard));
 }
 
-//
+// Full recipe card display function
+// Takes 1 argument, a detailed recipe object
+// Appends the completed recipe to the display area
+function buildIngredientsCard(recipe) {
+  var recipeTitle = recipe.title;
+  var recipeImage = recipeURL(recipe.id);
+  var recipeCard = $("<div class=\"recipe-card\"></div>");
+
+  $(recipeCard).append("<div class=\"recipe-card-thumbnail\"><img src=\"" + recipeImage + "\"></div>");
+  $(recipeCard).append("<h2 class=\"recipe-name\">" + recipeTitle + "</h2>");
+  $(recipeCard).append("<span class=\"recipe-source\">Recipe Source\"</span>");
+  $(recipeCard).append("<div class=\"recipe-ingredients\">Ingredients:<ul></ul>");
+  
+  // Appends each ingredient to an unordered list
+  for (var i = 0; i < recipe.extendedIngredients.length; i++) {
+    var ingredient = recipe.extendedIngredients[i];
+    var ingredientLI = $("<li class=\"ingredient-LI\">" + ingredient.original + "</li>");
+        
+    $(recipeCard).find("ul").append(ingredientLI);
+  }
+  $("#recipe-display").append(recipeCard);
+}
+
+// 
 $("#search-form").on("submit", function(event){
   event.preventDefault();
-  event.stopPropagation();
   var encodedQuery = encodeURI($("#search-input").val());
   var queryURL = "https://api.spoonacular.com/recipes/search?apiKey=" + apiKey + "&query=" + encodedQuery + "&number=10";
+  console.log("Submitted search with url: " + queryURL);
   searchResults = [];
   $.ajax({
     url: queryURL,
@@ -53,25 +76,38 @@ $("#search-form").on("submit", function(event){
       searchResults.push(response.results[i]);
     }
     console.log(searchResults);
-
+    $("#search-form").after("<div id=\"recipe-display\" class=\"row medium-8 large-7 columns\"></div>");
     for (var i = 0; i < searchResults.length; i++) {
       buildRecipeCard(searchResults[i]);
     }
-    
   })
 })
-
-$("#results").on("click", function(event) {
+$(document).on("click", ".fa-info-circle", function(event) {
   event.preventDefault();
   event.stopPropagation();
-
   console.log(event.target);
-
-  // Detects if whats clicked had a data attribute. Currently only the "View details" button should have a data-id attribute.
-  if($(event.target).attr("data-id")) {
-
-    // Uses the data attribute of the clicked button to build the query URL
+  alert("Clicked fa-info-circle");
     var queryURL = "https://api.spoonacular.com/recipes/" + $(event.target).attr("data-id") + "/information?includeNutrition=false&apiKey=" + apiKey;
+
+    $.ajax({
+      url: queryURL, 
+      method: "GET"
+    }).then(function(response) {
+      
+      console.log(response);
+
+      $("#recipe-display").remove();
+      $("#search-form").after("<div id=\"recipe-display\" class=\"row medium-8 large-7 columns\"></div>");
+      buildIngredientsCard(response); 
+    })
+})
+$(document).on("click", ".fa-plus", function(event) {
+  event.preventDefault();
+  event.stopPropagation();
+  console.log(event.target);
+  if($(event.target).hasClass("fa-plus")) {
+    alert("Clicked fa-plus");
+    // Uses the data attribute of the clicked button to build the query URL
 
     console.log(queryURL);
 
@@ -79,133 +115,10 @@ $("#results").on("click", function(event) {
       url: queryURL, 
       method: "GET"
     }).then(function(response) {
+      
       console.log(response);
-
-      // Supposed to clear the search results. Currently doesn't work
-      $("#results").remove();
-      $("#searchForm").after("<div id=\"results\"></div>");
-      $("#results").append("<p>Title: " + response.title + "</p>");
-      $("#results").append("<img src=\"" + recipeImage(response.id) + "\">");
-      $("#results").append("<p>Ingredients</p><br>");
-      for (var i = 0; i < response.extendedIngredients.length; i++) {
-        $("#results").append("<p>" + response.extendedIngredients[i].name + "</p>");
-      }
+      
     })
   }
 
-  
-
 })
-
-
-
-/*
-$.ajax({
-  url:recipiesWithIngredients,
-  method: "GET"
-}).then(function(response) {
-  console.log(response);
-})
-*/
-
-
-/*
-// displayMovieInfo function re-renders the HTML to display the appropriate content
-function displayMovieInfo() {
-
-  var movie = $(this).attr("data-name");
-  var queryURL = "https://www.omdbapi.com/?t=" + movie + "&apikey=trilogy";
-
-  // Creating an AJAX call for the specific movie button being clicked
-  $.ajax({
-    url: queryURL,
-    method: "GET"
-  }).then(function(response) {
-
-    // Creating a div to hold the movie
-    var movieDiv = $("<div class='movie'>");
-
-    // Storing the rating data
-    var rating = response.Rated;
-
-    // Creating an element to have the rating displayed
-    var pOne = $("<p>").text("Rating: " + rating);
-
-    // Displaying the rating
-    movieDiv.append(pOne);
-
-    // Storing the release year
-    var released = response.Released;
-
-    // Creating an element to hold the release year
-    var pTwo = $("<p>").text("Released: " + released);
-
-    // Displaying the release year
-    movieDiv.append(pTwo);
-
-    // Storing the plot
-    var plot = response.Plot;
-
-    // Creating an element to hold the plot
-    var pThree = $("<p>").text("Plot: " + plot);
-
-    // Appending the plot
-    movieDiv.append(pThree);
-
-    // Retrieving the URL for the image
-    var imgURL = response.Poster;
-
-    // Creating an element to hold the image
-    var image = $("<img>").attr("src", imgURL);
-
-    // Appending the image
-    movieDiv.append(image);
-
-    // Putting the entire movie above the previous movies
-    $("#movies-view").prepend(movieDiv);
-  });
-
-}
-
-// Function for displaying movie data
-function renderButtons() {
-
-  // Deleting the movies prior to adding new movies
-  // (this is necessary otherwise you will have repeat buttons)
-  $("#buttons-view").empty();
-
-  // Looping through the array of movies
-  for (var i = 0; i < movies.length; i++) {
-
-    // Then dynamicaly generating buttons for each movie in the array
-    // This code $("<button>") is all jQuery needs to create the beginning and end tag. (<button></button>)
-    var a = $("<button>");
-    // Adding a class of movie-btn to our button
-    a.addClass("movie-btn");
-    // Adding a data-attribute
-    a.attr("data-name", movies[i]);
-    // Providing the initial button text
-    a.text(movies[i]);
-    // Adding the button to the buttons-view div
-    $("#buttons-view").append(a);
-  }
-}
-
-// This function handles events where a movie button is clicked
-$("#add-movie").on("click", function(event) {
-  event.preventDefault();
-  // This line grabs the input from the textbox
-  var movie = $("#movie-input").val().trim();
-
-  // Adding movie from the textbox to our array
-  movies.push(movie);
-
-  // Calling renderButtons which handles the processing of our movie array
-  renderButtons();
-});
-
-// Adding a click event listener to all elements with a class of "movie-btn"
-$(document).on("click", ".movie-btn", displayMovieInfo);
-
-// Calling the renderButtons function to display the initial buttons
-renderButtons();*/
